03cd12465d055477be2f6d7d6759a376753182c5
IasminBrey16 <iasminbrey16@gmail.com>
2022-11-28 04:06:19 -0300

    .

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,17 @@ namespace Elementos_Quimicos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddCors(
+                options => {
+                    options.AddPolicy
+                    ("CorsPolicy", builder => builder.
+                        AllowAnyOrigin().
+                        AllowAnyHeader().
+                        AllowAnyMethod()
+                    );
+                }
+            );
+            
             services.AddDbContext<DataContext>
             (   
                 options => options.UseSqlite("DataSource=elements.db;Cache=shared") 
